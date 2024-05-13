abbac3da44015beb7edabf32cea9d065f9c6419c
mickaeltartar <mickael.tartar@hotmail.com>
2024-05-13 17:18:51 +0200

    get the status of the code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,26 +2,12 @@
 
 const request = require('request');
 
-// Function to make a GET request and display the status code
-function getStatus (url) {
-  // Making a GET request
-  request.get(url, function (error, response) {
-    if (error) {
-      console.error('Error occurred:', error);
-    } else {
-      // Displaying the status code
-      console.log('code:', response.statusCode);
-    }
-  });
-}
-
-// Taking the URL from command line argument
 const url = process.argv[2];
 
-// Checking if URL is provided
-if (url) {
-  // Calling the function to get the status code
-  getStatus(url);
-} else {
-  console.error('Please provide a URL as an argument.');
-}
+request(url, (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
